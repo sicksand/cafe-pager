28d3fa3bcf053689245d82c4dce7a128d9b7481d
Shafiq Mustapa <sicksand@gmail.com>
2023-09-06 09:51:37 +0800

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,7 +1,7 @@
-const websites = require('./website.json');
+cconst websites = require('./website.json');
 
 const randomIndex = Math.floor(Math.random() * websites.length);
 
 const randomWebsite = websites[randomIndex];
 
-document.getElementById('random-website').innerHTML = randomWebsite;
+export { randomWebsite };
