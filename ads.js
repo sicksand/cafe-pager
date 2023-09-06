3cd547b53a30328190e173fde2d9fbe3ee29e78e
Shafiq Mustapa <sicksand@gmail.com>
2023-09-06 20:57:56 +0800

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,6 +1,6 @@
 import websites from "./website.json" assert { type : "json" };
 //import data from "./data.json" assert { type: "json" };
-console.log(websites);
+//console.log(websites);
 //const websites = require('./website.json');
 
 const randomIndex = Math.floor(Math.random() * websites.length);
