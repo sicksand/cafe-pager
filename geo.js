cba004ef8954468e78f563e5774c8121ddd1f71b
Shafiq Mustapa <sicksand@gmail.com>
2023-09-21 14:13:01 +0800

    Update geo.js

--- geo.js
+++ geo.js
@@ -34,14 +34,15 @@ navigator.geolocation.getCurrentPosition(function(position) {
   // Proceed to the next page.
  } else {
   alert("Pokcik rasa korang takde kat cafe...");
-  document.write('<div class="alert">
+ 
+  document.write("<div class="alert">
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
   <span>we use cookies for no reason.</span>
   <div>
     <button class="btn btn-sm">Deny</button>
     <button class="btn btn-sm btn-primary">Accept</button>
   </div>
-</div>');
+</div>");
   // Redirects to the specified URL after 5 seconds
 setTimeout(function() {
   location.replace("https://linktr.ee/dapomokcikkopipokcik");
